e2d660c7798c39e99ac141cbc13972465c7865f2
reid <reid@scogg.in>
2022-05-02 15:20:42 -0500

    re-added download

--- buildmod.sh
+++ buildmod.sh
@@ -10,6 +10,9 @@ fi
 
 src="$(find /usr/src/app/wireguard-linux-compat-* -maxdepth 1 -type d -name 'src')"
 
+curl -fsSL "https://files.balena-cloud.com/images/${device_slug}/${os_version/+/%2B}/kernel_source.tar.gz" \
+	| tar xz --strip-components=2 -C /usr/src/app/
+
 # build wireguard kernel module
 make -C /usr/src/app/build modules_prepare -j"$(nproc)"
 make -C /usr/src/app/build M="${src}" -j"$(nproc)"
